aa63b8cbd89281806d0b1f882cbfde3fe77a0371
Wsine <weizhy3@hotmail.com>
2016-10-10 13:53:22 +0800

    finish server.config.sh

--- config/server.config.sh
+++ config/server.config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source server.config
-# echo $server_br0_bridge_ports
+eths=$(echo "$server_br0_bridge_ports" | tr ',' ' ')
 
 # check whether package is installed
 packages="dnsmasq bridge-utils"
@@ -14,14 +14,64 @@ for ((i=0; i<packagesNum; i++)) do
 		echo -e "\t${packagesArray[$i]} was installed."
 	else
 		echo -e "\t${packagesArray[$i]} was not installed. trying to install it..."
-		# apt-get install ${packagesArray[$i]}
+		apt-get install ${packagesArray[$i]}
 	fi
 done
 
+# bridge three Ethernets
+read -p "Please plugin the Ethernets network cards and press enter..."
+brctl addbr br0
+brctl addif br0 $eths
+
+# configurate network interfaces
+ethsArray=($eths)
+ethsNum=${#ethsArray[@]}
+ifaceEths=""
+for ((i=0; i<ethsNum; i++)) do
+	ifaceEths+="iface ${ethsArray[$i]} inet manual"
+	ifaceEths+=$'\n'
+done
+cat <<EOT > /etc/network/interfaces
+auto lo
+iface lo inet loopback
+
+auto eth0 $eths br0
+iface eth0 inet dhcp
+$ifaceEths
+# Bridge setup
+iface br0 inet static
+    bridge_ports $eths
+    address $server_br0_address
+    netmask $server_br0_netmask
+    network $server_br0_network
+    broadcast $server_br0_broadcast
+
+auto wlan0
+EOT
+
+# configurate dnsmasq
+cat <<EOT > /etc/dnsmasq.conf
+# Never forward plain names (without a dot or domain part)
+domain-needed
+# Never forward addresses in the non-routed address spaces.
+bogus-priv
+
+# listen-interface
+interface=$server_interface
+# listen on by address
+listen-address=$server_listen_address
+# nameserver
+bind-interfaces
+server=$server_nameserver
+
+# supply DHCP service
+dhcp-range=$server_dhcp_range
+EOT
+
 # enable ip forward
-# sysctl -w net.ipv4.ip_forward=1
+sysctl -w net.ipv4.ip_forward=1
 
 # configurate br0 with nat network
-# iptables -t nat -A POSTROUTING -o br0 -j MASQUERADE
-# iptables -A FORWARD -i br0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-# iptables -A FORWARD -i wlan0 -o br0 -j ACCEPT
+iptables -t nat -A POSTROUTING -o br0 -j MASQUERADE
+iptables -A FORWARD -i br0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i wlan0 -o br0 -j ACCEPT
