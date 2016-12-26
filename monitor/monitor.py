4d6aa6a87c999654b04cd9ff1cd5bb2b38d79e23
Wsine <weizhy3@hotmail.com>
2016-12-26 22:09:02 +0800

    update monitor

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 '''
@@ -6,15 +7,17 @@ function: monitor the cluster state periodically
           save recent data into local database
 '''
 
-import httplib
-import json
-import sys
-import time
-import threading
-from multiprocessing import Process, Manager, Lock, Pool
-import MySQLdb
+import httplib, json, sys, time
+from multiprocessing import Pool
+from pymongo import MongoClient
 
 PORT = 4243
+DB_CONFIG = {
+	"host" : "localhost",
+	"port" : 27017,
+	"user" : "datexla",
+	"pwd"  : "datexla"
+}
 
 # return type: node map, whose data is in the format <- "node ip": (node id, node name) ->
 def collect_worker_info(manager_ip):
@@ -44,60 +47,45 @@ def probe_node(node_ip):
 	response = conn.getresponse()
 	data = response.read()
 	json_data = json.loads(data)
-
-	if len(json_data) == 0:
-		return
-
-	calc_time = json_data["calcTime"]
-	cpu_score = json_data["cpuScore"]
-	mem_score = json_data["memScore"]
-	score = json_data["score"]
-	node_id = node_map[node_ip][0]
-	node_name = node_map[node_ip][1]
-	result = (calc_time, node_name, node_id, score, cpu_score, mem_score)
-
-	return result
+	json_data["hostName"] = node_map[node_ip][1]
+	json_data["nodeID"] = node_map[node_ip][0]
+	json_data["ip"] = node_ip
+	return json_data
 
 if __name__ == "__main__":
 	#input validation
 	if len(sys.argv) != 2:
-		print "Usage: monitor.py Manager IP Address"
+		print "Usage: monitor.py Manager_IP_Address"
 		sys.exit(1)
 	manager_ip = sys.argv[1]
 
 	# collect all workers' ip addressess
 	node_map = collect_worker_info(manager_ip)
-	conn = MySQLdb.connect(user='root', passwd='crash', host='127.0.0.1', db='datexla_dcn')
+	client = MongoClient(DB_CONFIG["host"], DB_CONFIG["port"])
+	db = client.datexla
+	collection = db.cluster
 	
-	while True:
-		# monitor the cluster
-		pool = Pool(len(node_map))
-		result_list = []
-		for ip in node_map:
-			result_list.append(pool.apply_async(probe_node, [ip]))
-		pool.close()
-		pool.join()
-
-		# save data and write log
-		log_file = open('cluster_score.log', 'a')
-		cur = conn.cursor()
-		sql_format = 'insert into ClusterInfo values(%s, %s, %s, %s, %s);'
-		insert_values = []
-		for res in result_list:
-			r = res.get()
-			calc_time, node_name, node_id, score, cpu_score, mem_score = r[0], r[1], r[2], r[3], r[4], r[5]
-			# write log
-			log_file.write("Calc_Time: %s Name: %s ID: %s Score: %f CPU_Score: %f Mem_Score: %f\n" % (calc_time, node_name, node_id, score, cpu_score, mem_score))
-			# save data into database
-			curr_value = (calc_time, node_id, score, cpu_score, mem_score)
-			insert_values.append(curr_value)
-			# print for debugging
-			print("Calc_Time: %s Name: %s ID: %s Score: %f CPU_Score: %f Mem_Score: %f" % (calc_time, node_name, node_id, score, cpu_score, mem_score))
-		cur.executemany(sql_format, insert_values)
-		cur.close()
-		conn.commit()
-	
-		time.sleep(7)
-	# close database and log file
-	conn.close()
-	log_file.close()
+	with open('cluster_score.log', 'w') as log_file:
+		while True:
+			# monitor the cluster
+			pool = Pool(len(node_map))
+			result_list = []
+			for ip in node_map:
+				result_list.append(pool.apply_async(probe_node, [ip]))
+			pool.close()
+			pool.join()
+			# save data and write log
+			insert_values = []
+			for res in result_list:
+				r = res.get()
+				if "score" not in r:
+					continue
+				# save to database
+				insert_values.append(r)
+				# save to log file
+				log = "%s [score print] hostName: %s, score: %f, cpuScore: %f, memScore: %f, nodeID: %s, ip: %s\n" % (r["calcTime"], r["hostName"], r["score"], r["cpuScore"], r["memScore"], r["nodeID"], r["ip"])
+				log_file.write(log)
+				print(log)
+			# insert into database
+			collection.insert(insert_values)
+			time.sleep(7)
